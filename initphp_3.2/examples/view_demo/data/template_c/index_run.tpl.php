a6f260751b851231c03b1d8f4b49dceacbba379e
zhuli <420332292@qq.com>
2012-08-31 14:21:23 +0800

    Revert "initphp 3.2"
    This reverts commit d4393dedc0d36c2d8679a8bb524fd22d002601d7.

--- initphp_3.2/examples/view_demo/data/template_c/index_run.tpl.php
+++
@@ -1,2 +0,0 @@
-<?php  if (!defined("IS_INITPHP")) exit("Access Denied!");  /* INITPHP Version 1.0 ,Create on 2012-05-31 11:10:56, compiled from /var/www/initphp_32/examples/view_demo/web/template/index_run.htm */ ?>
-<?php echo $title; ?>
