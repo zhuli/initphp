701598dcb191ddcd8d7602faf68d36ebecbeb5d1
zhuli.zhul <zhuli.zhul@alibaba-inc.com>
2015-06-23 10:37:18 +0800

    删除老版本

--- demo/app/data/template_c/demo/view_tpl_1.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php  if (!defined("IS_INITPHP")) exit("Access Denied!");  /* INITPHP Version 1.0 ,Create on 2014-11-25 08:08:04, compiled from ../app/web/template/demo/view_tpl_1.htm */ ?>
-tpl_1
-<br/>
